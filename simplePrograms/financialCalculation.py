aab126ead5072f4e5eca7f0353ad82890f922920
intarpit <int.arpit@gmail.com>
2020-11-30 13:37:45 -0600

    Update carLoanCalculation code

--- simplePrograms/financialCalculation.py
+++ simplePrograms/financialCalculation.py
@@ -27,19 +27,26 @@ def simpleInterest(amount, rate, term):
 Total amount to be paid in {term} years = ${totalAmount:,d}
 Monthly Payment = ${monthlyPayment:,d}\n""")
 
-def carLoanCalculator(amount, rate, term):
+def carLoanCalculator():
     """
     This function calculates compound interest.
     Rate is assumed annunally and term is assumed monthly.
     """
-    rate = rate/(100 * 12)
-    discountRate = (((1 + rate) ** term) - 1) / (rate * (1 + rate) ** term)
-    monthlyPayment = (amount / discountRate)
-    monthlyPaymentInterger = int(round(amount / discountRate))
-    totalCost = int(round(monthlyPayment * term))
-    interest = totalCost - amount
+    while True:
+        try:
+            amount = int(input("Please enter the loan amount (avoid comma): "))
+            rate = float(input("Please enter the rate of interest: "))
+            term = int(input("Please eneter the number of months: "))
+        except ValueError:
+            continue
+        rate = rate/(100 * 12)
+        discountRate = (((1 + rate) ** term) - 1) / (rate * (1 + rate) ** term)
+        monthlyPayment = (amount / discountRate)
+        monthlyPaymentInterger = int(round(amount / discountRate))
+        totalCost = int(round(monthlyPayment * term))
+        interest = totalCost - amount
     
-    return (f"""Interest to be paid in {term} months = ${interest:,d}
+        return (f"""\nInterest to be paid in {term} months = ${interest:,d}
 Total amount to be paid in {term} months = ${totalCost:,d}
 Monthly Payment = ${monthlyPaymentInterger:,d}\n""")
 
@@ -63,5 +70,5 @@ Monthly Payment = ${monthlyPaymentInterger:,d}\n""")
 
 # print(compoundInterest(amount, rate, term)) #! Please add value of your choice instead of amount, rate and term (please enter term in years).
 # print(simpleInterest(amount, rate, term)) #! Please add value of your choice instead of amount, rate and term (please enter term in years).
-# print(carLoanCalculator(amount, rate, term)) #! Please add value of your choice instead of amount, rate and term (please enter term in months).
+# print(carLoanCalculator()) #! Please comment it out and run the program.
 # print(carLoanCalculator(amount, rate, term)) #! Please add value of your choice instead of amount, rate and term (please enter term in years).
